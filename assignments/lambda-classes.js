2f317a4d74297e2149c4769f765e43a1a5c60801
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-25 16:16:44 -0500

    Student child class constructor and methods

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -4,17 +4,39 @@ class Person {
         this.name = attributes.name;
         this.age = attributes.age;
         this.location = attributes.location;
-    }
+    } //Constructor Class
     speak(){
         console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
     }
 }
 
-class Instructor {
+class Instructor extends Person {
     constructor(attributes){
+        super(attributes);
         this.specialty = attributes.specialty;
         this.favLanguage = attributes.favLanguage;
         this.catchPhrase = attributes.catchPhrase;
+    }// Instructor (child) Methods
+    demo(subject){
+        return (`Today we are learning about ${this.subject}`);
     }
-    
+    grade(student){
+        return (`${this.name} receives a perfect score on ${this.subject}`);
+    }
 }
+
+class Student extends Person {
+    constructor(attributes){
+        super(attributes);
+        this.previousBackground = attributes.previousBackground;
+        this.className = attributes.className;
+        this.favSubjects = attributes.favSubjects;
+    }
+    listsSubjects(favSubjects){
+        return favSubjects;
+    }
+    PRAssignments(subject){
+        return `${this.name} has submitted a pull request for ${this.subject}`
+    }
+}
+
