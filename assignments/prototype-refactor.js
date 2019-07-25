e0530300391d040268ec163ce7ade8e5f7cd4b60
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-25 15:36:46 -0500

    created Person class

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -10,8 +10,8 @@ Prototype Refactor
 class GameObject {
     constructor(attributes){
         this.newCreatedAt = attributes.createdAt;
-        this.newName = attrs.name;
-        this.newDimensions = attrs.dimensions;
+        this.newName = attributes.name;
+        this.newDimensions = attributes.dimensions;
     }
 
     destroy(){
@@ -22,8 +22,9 @@ class GameObject {
 
   class CharacterStats extends GameObject {
       constructor(attributes) {
-        this.newHealthPoints = attributes.healthPoints;
         super(attributes);
+        this.newHealthPoints = attributes.healthPoints;
+        
       }
       takeDamage(){
         return (`${this.newName} took damage.`);
@@ -32,12 +33,80 @@ class GameObject {
 
     class Humanoid extends CharacterStats {
     constructor(attributes){
+        super(attributes);
         this.newTeam = attributes.team;
         this.newWeapons = attributes.weapons;
         this.newLanguage = attributes.language;
-        super(attributes);
+        
     }   
         greet(){
             return (`${this.newName} offers a greeting in ${this.newLanguage}.`)
         }     
     }
+
+    const mage = new Humanoid({
+        createdAt: new Date(),
+        dimensions: {
+          length: 2,
+          width: 1,
+          height: 1,
+        },
+        healthPoints: 5,
+        name: 'Bruce',
+        team: 'Mage Guild',
+        weapons: [
+          'Staff of Shamalama',
+        ],
+        language: 'Common Tongue',
+      });
+    
+      const swordsman = new Humanoid({
+        createdAt: new Date(),
+        dimensions: {
+          length: 2,
+          width: 2,
+          height: 2,
+        },
+        healthPoints: 15,
+        name: 'Sir Mustachio',
+        team: 'The Round Table',
+        weapons: [
+          'Giant Sword',
+          'Shield',
+        ],
+        language: 'Common Tongue',
+      });
+    
+      const archer = new Humanoid({
+        createdAt: new Date(),
+        dimensions: {
+          length: 1,
+          width: 2,
+          height: 4,
+        },
+        healthPoints: 10,
+        name: 'Lilith',
+        team: 'Forest Kingdom',
+        weapons: [
+          'Bow',
+          'Dagger',
+        ],
+        language: 'Elvish',
+      });
+    
+      console.log(mage.newCreatedAt); // Today's date
+      console.log(archer.newDimensions); // { length: 1, width: 2, height: 4 }
+      console.log(swordsman.newHealthPoints); // 15
+      console.log(mage.newName); // Bruce
+      console.log(swordsman.newTeam); // The Round Table
+      console.log(mage.newWeapons); // Staff of Shamalama
+      console.log(archer.newLanguage); // Elvish
+      console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+      console.log(mage.takeDamage()); // Bruce took damage.
+      console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
+    
+    
+      // Stretch task: 
+      // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
+      // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+      // * Create two new objects, one a villain and one a hero and fight it out with methods!
