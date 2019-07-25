e5f3190f5ca0d2d743f0dfed11883c8eb1239e5c
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-25 14:46:04 -0500

    refactored GameObject into class

--- assignments/prototype-refactor.js
+++ assignments/prototype-refactor.js
@@ -7,3 +7,17 @@ Prototype Refactor
 2. Your goal is to refactor all of this code to use ES6 Classes. The console.log() statements should still return what is expected of them.
 
 */
+class GameObject {
+    constructor(attrs){
+        this.createdAt = attrs.createdAt;
+        this.name = attrs.name;
+        this.dimensions = attrs.dimensions;
+    }
+
+    destroy(){
+        return `${this.name} was removed from the game.`
+      }
+  };
+
+
+
