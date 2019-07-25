c90b2eb9c424805da79bbffd52bd3a5f1b22136b
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-07-25 16:04:45 -0500

    demo and grade methods added

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,20 @@
 // CODE here for your Lambda Classes
+class Person {
+    constructor(atrributes){
+        this.name = attributes.name;
+        this.age = attributes.age;
+        this.location = attributes.location;
+    }
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}.`);
+    }
+}
+
+class Instructor {
+    constructor(attributes){
+        this.specialty = attributes.specialty;
+        this.favLanguage = attributes.favLanguage;
+        this.catchPhrase = attributes.catchPhrase;
+    }
+    
+}
